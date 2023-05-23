734c17276c221806b97aa752eb2c8c8b23b297b1
Krzysztof Korzela <kkorzela@redhat.com>
2023-05-23 11:00:01 +0200

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$dir = 'images/';
+$images = glob($dir . '*.jpg');
+
+if (empty($images)) {
+    echo 'Brak obrazków do wyświetlenia.';
+} else {
+    foreach ($images as $image) {
+        echo '<img src="' . $image . '" alt="Obrazek" /><br />';
+    }
+}
+?>
